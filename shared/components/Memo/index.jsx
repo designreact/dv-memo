bd914b0675bda664e07512ad44ce8ad2ea1c94b1
martinking <bitbucket@designreact.co.uk>
2017-04-19 18:10:47 +0100

    add Memo and unit tests

--- shared/components/Memo/index.jsx
+++ shared/components/Memo/index.jsx
@@ -3,12 +3,43 @@ if (process.env.CLIENT) {
 }
 
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
+import DynamicField from '../DynamicField'
 
-const Memo = () => {
+const Memo = ({ id, title, body, isNew, onUpdate, onDelete }) => {
+  const memo = { id, title, body }
+  const updateMemo = (className, value) => {
+    // update memo ahead sending to server
+    if (className === 'memo-title') {
+      memo.title = value
+    } else {
+      memo.body = value
+    }
+    onUpdate(memo)
+  }
+  const deleteMemo = () => {
+    onDelete(memo)
+  }
   return (
-    <div className="memo" />
+    <div className="memo">
+      {<DynamicField TagName="h4" className="memo-title" value={title} focus={isNew} onUpdateValue={updateMemo} />}
+      {<DynamicField TagName="p" className="memo-body" value={body} onUpdateValue={updateMemo} />}
+      <button className="memo-delete" onClick={deleteMemo}>Delete</button>
+    </div>
   )
+}
+
+Memo.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  isNew: PropTypes.bool,
+  onUpdate: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+}
+
+Memo.defaultProps = {
+  isNew: false,
 }
 
 export default Memo
---
+++ shared/components/Memo/test/index.spec.js
@@ -0,0 +1,54 @@
+/* eslint react/jsx-filename-extension:0 */
+/* eslint no-unused-expressions:0 */
+
+import React from 'react'
+import { mount } from 'enzyme'
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import Memo from '../index'
+import DynamicField from '../../DynamicField'
+
+chai.use(chaiEnzyme())
+
+describe('Given <Memo />', () => {
+  let props
+  let output
+  beforeEach(() => {
+    props = {
+      id: 0,
+      title: 'A title',
+      body: 'A memo body',
+      isNew: false,
+      onUpdate: sinon.spy(),
+      onDelete: sinon.spy(),
+    }
+    output = mount(<Memo {...props} />)
+  })
+  afterEach(() => {
+    output = null
+  })
+  it('Should render div.memo', () => {
+    expect(output).to.have.className('memo')
+  })
+  it('Should render 2 DynamicFields', () => {
+    expect(output).to.have.exactly(2).descendants(DynamicField)
+  })
+  it('Should render a button.memo-delete', () => {
+    expect(output).to.have.exactly(1).descendants('button.memo-delete')
+  })
+  it('Should call props.onUpdate', () => {
+    // convert to input
+    output.find('h4.memo-title').simulate('focus')
+    // fire blur event on input
+    output.find('input.memo-title').simulate('blur')
+    // convert to input
+    output.find('p.memo-body').simulate('focus')
+    // fire blur event on input
+    output.find('input.memo-body').simulate('blur')
+    expect(props.onUpdate.calledTwice).to.be.true
+  })
+  it('Should call props.onDelete', () => {
+    output.find('button.memo-delete').simulate('click')
+    expect(props.onDelete.calledOnce).to.be.true
+  })
+})
